ddae9c355de63a533a6d41559d34cbcb261bce3f
Troy Murray <tm@msu.edu>
2011-10-31 17:08:40 -0400

    Fixed bad URL for Tomcat, replaced with another host to pull from

--- setup-railo.sh
+++ setup-railo.sh
@@ -82,7 +82,7 @@ sudo ln -s /usr/local/java/latest/bin/java /usr/local/bin/java
 
 
 # Download and Install Apache Tomcat server
-sudo wget http://mirrors.axint.net/apache/tomcat/tomcat-7/v7.0.TOMCAT_VERSION/bin/apache-tomcat-7.0.TOMCAT_VERSION.tar.gz
+sudo wget http://apache.petsads.us/tomcat/tomcat-7/v7.0.$TOMCAT_VERSION/bin/apache-tomcat-7.0.$TOMCAT_VERSION.tar.gz
 sudo tar -xvzf apache-tomcat-$TOMCAT_VERSION.tar.gz
 sudo mv apache-tomcat-$TOMCAT_VERSION /opt/tomcat
 sudo rm -Rf apache-tomcat-$TOMCAT_VERSION.tar.gz
