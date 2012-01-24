8d2a7bec5d4834769b6bead82cb6d044c5882d92
Troy Murray <troy.murray@gmail.com>
2012-01-24 09:00:13 -0500

    Updated script for Apache Tomcat 7.0.25

--- setup-railo.sh
+++ setup-railo.sh
@@ -2,7 +2,7 @@
 
 # Automated 64-bit Ubuntu Railo Application Server Setup
 # Setup variables
-TOMCAT_VERSION="7.0.23"
+TOMCAT_VERSION="7.0.25"
 JAVA_MINOR_VERSION="30"
 RAILO_VERSION="3.3.1.000"
 
@@ -82,7 +82,7 @@ sudo ln -s /usr/local/java/latest/bin/java /usr/local/bin/java
 
 
 # Download and Install Apache Tomcat server
-sudo wget http://ftp.wayne.edu/apache/tomcat/tomcat-7/v7.0.$TOMCAT_VERSION/bin/apache-tomcat-7.0.$TOMCAT_VERSION.tar.gz
+sudo wget http://apache.cs.utah.edu/tomcat/tomcat-7/v7.0.$TOMCAT_VERSION/bin/apache-tomcat-7.0.$TOMCAT_VERSION.tar.gz
 sudo tar -xvzf apache-tomcat-$TOMCAT_VERSION.tar.gz
 sudo mv apache-tomcat-$TOMCAT_VERSION /opt/tomcat
 sudo rm -Rf apache-tomcat-$TOMCAT_VERSION.tar.gz
